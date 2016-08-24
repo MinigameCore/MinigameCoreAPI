3c45aa3dc06ae22e7cb1e5d1da09be2f1c5ebe72
kinggoesgaming <hunar.roop@gmail.com>
2016-08-23 20:42:17 -0700

    add Configuration

---
+++ src/main/java/io/github/minigamecore/api/util/config/Configuration.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of MinigameCoreAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 - 2016 MinigameCore <http://minigamecore.github.io>
+ * Copyright (c) Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.github.minigamecore.api.util.config;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+
+/**
+ *
+ */
+public final class Configuration {
+
+    private final ConfigurationLoader<CommentedConfigurationNode> loader;
+    private CommentedConfigurationNode node;
+
+    public Configuration(Supplier<HoconConfigurationLoader> loaderSupplier) {
+        checkNotNull(loaderSupplier, "configuration loader supplier");
+        this.loader = loaderSupplier.get();
+    }
+
+    public void load() throws IOException {
+        node = loader.load();
+    }
+
+    public CommentedConfigurationNode get() {
+        return node;
+    }
+
+    public void save() throws IOException {
+        loader.save(node);
+    }
+
+}
